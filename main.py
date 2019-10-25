2568d2eb7e3d18bbc1b510dec727eec4e45c41b7
KhachkovSemen2003 <44490927+KhachkovSemen2003@users.noreply.github.com>
2019-10-25 08:17:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,55 @@
 class triangle: #Akmal
-  pass
+   def __init__(self, x, y, size, color):
+        self.x = x
+        self.y = y
+        self.size = size
+        self.color = color
+        
+    def show(self):
+        turtle.up()
+        turtle.goto(self.x, self.y)
+        turtle.color(self.color)
+        turtle.down()
+        for _ in range (3):
+            turtle.forward(self.size)
+            turtle.left(60)
+        turtle.begin_fill()
+
 class squire: #Dima
-  pass
+    def __init__(self, x, y, size, color):
+        self.x = x
+        self.y = y
+        self.size = size
+        self.color = color
+
+    def show(self):
+        turtle.up()
+        turtle.goto(self.x, self.y)
+        turtle.color(self.color)
+        turtle.down()
+        for _ in range (4):
+            turtle.forward(self.size)
+            turtle.left(90)
+        turtle.begin_fill()
+
 class parallelogram: #Semen
-  pass
+  class Parallelogram:
+    def __init__(self, x, y, size, length, color, angle):
+        self.x = x
+        self.y = y
+        self.size = size
+        self.color = color
+        self.angle = angle
+        self.length = length
+    def show(self):
+        turtle.up()
+        turtle.goto(self.x, self.y)
+        turtle.color(self.color)
+        turtle.down()
+        for _ in range (2):
+            turtle.forward(self.size)
+            turtle.left(self.angle)
+            turtle.forward(self.length)
+            turtle.left(180-self.angle)
+        turtle.begin_fill()
+
