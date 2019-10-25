e297db99b0773a500bf04fe5208d32af09800f75
Akmal Umarov <44490997+Akmalumarov@users.noreply.github.com>
2019-10-25 09:55:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,26 @@
 import turtle
 class triangle: #Akmal
-   def __init__(self, x, y, size, color):
+class Triangle(object):      #AKMAL
+    def __init__(self,x,y,side,color,angle):
+        self.color = color
         self.x = x
         self.y = y
-        self.size = size
-        self.color = color
-        
-    def show(self):
-        turtle.up()
-        turtle.goto(self.x, self.y)
-        turtle.color(self.color)
-        turtle.down()
-        for _ in range (3):
-            turtle.forward(self.size)
-            turtle.left(60)
-        turtle.begin_fill()
-
-class squire: #Dima
-    def __init__(self, x, y, size, color):
-        self.x = x
-        self.y = y
-        self.size = size
-        self.color = color
-
-    def show(self):
-        turtle.up()
-        turtle.goto(self.x, self.y)
-        turtle.color(self.color)
-        turtle.down()
-        for _ in range (4):
-            turtle.forward(self.size)
-            turtle.left(90)
-        turtle.begin_fill()
-     
-
-class parallelogram: #Semen
-  class Parallelogram:
-    def __init__(self, x, y, size, length, color, angle):
-        self.x = x
-        self.y = y
-        self.size = size
-        self.color = color
+        self.side = side
         self.angle = angle
-        self.length = length
-    def show(self):
-        turtle.up()
-        turtle.goto(self.x, self.y)
-        turtle.color(self.color)
-        turtle.down()
-        for _ in range (2):
-            turtle.forward(self.size)
-            turtle.left(self.angle)
-            turtle.forward(self.length)
-            turtle.left(180-self.angle)
-        turtle.begin_fill()
 
+    def draw(self):
+              turtle.up()
+              turtle.color(self.color)
+              turtle.right(self.angle)
+              turtle.goto(self.x, self.y)
+              turtle.down()
+              turtle.fillcolor(self.color)
+              turtle.begin_fill()
+              turtle.forward(self.side)
+              turtle.left(120)
+              turtle.forward(self.side)
+              turtle.left(120)
+              turtle.forward(self.side)
+              turtle.left(120)
+              turtle.end_fill()
+              turtle.hideturtle()
