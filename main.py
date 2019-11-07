c532c4ef7da8ce00b6aa5bb127c3b045561957e8
Akmal Umarov <44490997+Akmalumarov@users.noreply.github.com>
2019-11-07 14:36:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import turtle
-class triangle: #Akmal
+
 class Triangle(object):      #AKMAL
     def __init__(self,x,y,side,color,angle):
         self.color = color
@@ -9,18 +9,21 @@ class Triangle(object):      #AKMAL
         self.angle = angle
 
     def draw(self):
-              turtle.up()
-              turtle.color(self.color)
-              turtle.right(self.angle)
-              turtle.goto(self.x, self.y)
-              turtle.down()
-              turtle.fillcolor(self.color)
-              turtle.begin_fill()
-              turtle.forward(self.side)
-              turtle.left(120)
-              turtle.forward(self.side)
-              turtle.left(120)
-              turtle.forward(self.side)
-              turtle.left(120)
-              turtle.end_fill()
-              turtle.hideturtle()
+        turtle.up()
+        turtle.color(self.color)
+        turtle.right(self.angle)
+        turtle.goto(self.x, self.y)
+        turtle.down()
+        turtle.fillcolor(self.color)
+        turtle.begin_fill()
+        turtle.forward(self.side)
+        turtle.left(120)
+        turtle.forward(self.side)
+        turtle.left(120)
+        turtle.forward(self.side)
+        turtle.left(120)
+        turtle.end_fill()
+        turtle.hideturtle()
+
+tri1 = Triangle(90, 90, 100, 'green', 0)
+tri1.draw()
